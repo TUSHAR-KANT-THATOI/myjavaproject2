d18af6d9a365d5b1e5beccaf373645d2f5ecd3e9
Tushar-Kant-Thatoi <thatoi.tusharkant500@gmail.com>
2025-10-14 02:41:12 -0700

    oop

---
+++ test12.java
@@ -0,0 +1,10 @@
+class Employee {
+    static String compname = "seere";
+}
+
+public class test12 {
+    public static void main(String[] args) {
+        System.out.println(Employee.compname);
+    }
+
+}
