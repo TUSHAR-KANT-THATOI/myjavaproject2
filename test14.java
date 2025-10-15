0c4e0a5180317474d8a67d8304d4e5087c4cd551
Tushar-Kant-Thatoi <thatoi.tusharkant500@gmail.com>
2025-10-15 11:43:26 -0700

    oop

---
+++ test14.java
@@ -0,0 +1,12 @@
+class student {
+    int roll;
+}
+
+public class test14 {
+    public static void main(String[] args) {
+        student myobj = new student();
+        myobj.roll = 40;
+        System.out.println(myobj.roll);
+    }
+
+}
